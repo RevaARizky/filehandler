44a1469c3aab4887381a6c57b70ba99c49758ebe
RevaARizky <revaathallah86@gmail.com>
2024-06-30 15:33:44 +0800

    0.0.1

--- filehandler.sh
+++ filehandler.sh
@@ -10,6 +10,24 @@ is_wsl() {
     fi
 }
 
+
+get_os() {
+    os_type=$(uname)
+
+    if [ "$os_type" == "Darwin" ]; then
+        return "mac"
+
+    elif [ "$os_type" == "Linux" ]; then
+        if is_wsl; then
+            return "wsl"
+        else 
+            return "linux"
+        fi
+    else
+        return $os_type
+    fi
+}
+
 fixedpath="/mnt/d/dev/test/Clients"
 pathshortcut="/mnt/d/dev/test2"
 # if find $fixedpath -name $1 -type d
@@ -19,54 +37,28 @@ rm -R $pathshortcut/short/*
 for i in $(find $fixedpath -name $1); do # Not recommended, will break on whitespace
     result=$(echo "$i" | sed "s|$fixedpath||g")
     result=$(echo "$result" | sed "s|/|_|g")
-    echo "$result"
     newfolder="$pathshortcut/short/$result"
     mkdir $newfolder
 
-    osascript -e "
-    set originalFolderAlias to POSIX file $i as alias
-    set destinationFolderAlias to POSIX file $newfolder as alias
-    tell application \"Finder\"
-       make new alias to originalFolderAlias at destinationFolderAlias
-    end tell
-    "
+    if [ $(get_os) == "mac" ]; then
+        osascript -e "
+        set originalFolderAlias to POSIX file $i as alias
+        set destinationFolderAlias to POSIX file $newfolder as alias
+        tell application \"Finder\"
+            make new alias to originalFolderAlias at destinationFolderAlias
+        end tell
+        "
+    fi
 done
-os_type=$(uname)
 
-if [ "$os_type" == "Darwin" ]; then
-    echo "Running on macOS"
+os=$(get_os)
+
+if [ "$os" == "mac" ]; then
     open $pathshortcut/short
-    # macOS-specific commands here
 
-elif [ "$os_type" == "Linux" ]; then
-    if is_wsl; then
-        explorer.exe `wslpath -w "$pathshortcut/short"`
-    else 
-        cd $pathshortcut/short
-    # Linux-specific commands here
+elif [ "$os" == "linux" ]; then
+    cd $pathshortcut/short
 
-else
-    echo "Unknown Operating System: $os_type"
-    # Other OS-specific commands here
+elif [ "$os" == "wsl" ]; then
+    explorer.exe `wslpath -w "$pathshortcut/short"`
 fi
-explorer.exe `wslpath -w "$pathshortcut/short"`
-cd "$pathshortcut/short"
-# fi
-
-
-# if [[ $# -ne 2 ]]; then
-#     echo "mkalias: specify 'from' and 'to' paths" >&2
-#     exit 1
-# fi
-
-# from="$(realpath $1)"
-# todir="$(dirname $(realpath $2))"
-# toname="$(basename $(realpath $2))"
-# if [[ -f "$from" ]]; then
-#     type="file"
-# elif [[ -d "$from" ]]; then
-#     type="folder"
-# else
-#     echo "mkalias: invalid path or unsupported type: '$from'" >&2
-#     exit 1
-# fi
